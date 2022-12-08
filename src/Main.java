efc4cb40187b8bfcc2b94fca45578222316ac5ed
adilet44 <adiletusupbekov20@gmail.com>
2022-12-08 17:28:27 +0300

    изменил д/з

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,28 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         double[] aspire = {12.1, -5.61, 45.4, -74.8, 42.5, -6.6, 2.3, -9.09, 1.17, -24.9, 26.6, -4.35, 7.5, -6.4, 6.9};
-        double resultOfAspire = 0.0;
-        for (double aspare : aspire) {
-            if (aspare < 0) {
-                continue;
+        double skrin = 0;
+        int snik = 0;
+        boolean asir = false;
+        for (double go : aspire) {
+            if (go < 0) {
+                asir = true;
+            } else if (go > 0 && asir) {
+                skrin = go;
+                snik++;
+                System.out.println(go);
+
+
             }
-            resultOfAspire += aspare;
         }
-        System.out.println(resultOfAspire);
-       
+        System.out.println("среднее орифматическое число: " + skrin / snik);
     }
 
-
-
 }
+
+
+
+
 
 
 
